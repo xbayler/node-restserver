1e8784853584b4431b3c85fa7a4837569ad81204
Sergio <sergio.ascanio.94@gmail.com>
2018-11-24 16:25:27 +0100

    Primer commit

---
+++ server/server.js
@@ -0,0 +1,49 @@
+require("./config/config");
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/', function(req, res) {
+    res.json("Hello World");
+});
+
+app.get('/usuario', function(req, res) {
+    res.json('GET USUARIO');
+});
+
+app.post('/usuario', function(req, res) {
+
+    let body = req.body;
+
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario'
+        });
+    } else {
+        res.json(body);
+    }
+});
+
+app.put('/usuario/:id', function(req, res) {
+
+    let id = req.params.id;
+
+    res.json({
+        id,
+        mensaje: "PUT USUARIO"
+    });
+});
+
+app.delete('/usuario', function(req, res) {
+    res.json("DELETE USUARIO");
+});
+
+app.listen(process.env.PORT, () => console.log(`Escuchando el puerto ${process.env.PORT }`));
