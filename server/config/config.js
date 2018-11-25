296fe0d3f247f0b82e88fa80a775219cf6cd6288
Sergio <sergio.ascanio.94@gmail.com>
2018-11-25 13:31:04 +0100

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@
 process.env.PORT = process.env.PORT || 3000;
 
 // Entorno //
-process.env.NODE_ENV = process.env.NODE_ENV || "dev";
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // Entorno BBDD //
 let urlDB;
@@ -10,7 +10,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = "mongodb://cafe-user:cafe123@ds159387.mlab.com:59387/cafe";
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
